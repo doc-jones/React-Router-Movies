84dfd73d8d9a70843be82e1b2a7c88217e4de896
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-07 15:41:04 -0400

    Add Route(s) to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -21,9 +21,15 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <SavedList list={this.state.savedList} 
+        removeFromSavedList={this.removeFromSavedList} />
+        <Route exact path="/" render={(props) => <MovieList {...props} addToSavedList={this.addToSavedList} />} >
+        </Route>
+        <Route>
+          path="/movies/:id"
+          render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />} >
+        </Route>
       </div>
-    );
+    )
   }
 }
