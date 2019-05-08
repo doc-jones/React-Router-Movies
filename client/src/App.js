077b1f368e0bd47370830f0e63ad839b7fd2884c
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-08 11:43:00 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import Movie from './Movies/Movie'
 
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor(){
+    super()
     this.state = {
       savedList: []
-    };
+    }
   }
 
-  addToSavedList = movie => {
+  addToSavedList = (movie) => {
+    console.log(this.state.savedList)
     const savedList = this.state.savedList;
     savedList.push(movie);
-    this.setState({ savedList });
-  };
+    this.setState({savedList});
+  }
 
-  render() {
+  render(){
     return (
       <div>
-        <SavedList list={this.state.savedList} 
-        removeFromSavedList={this.removeFromSavedList} />
-        <Route exact path="/" render={(props) => <MovieList {...props} addToSavedList={this.addToSavedList} />} >
-        </Route>
-        <Route>
-          path="/movies/:id"
-          render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />} >
-        </Route>
+        <SavedList list={this.state.savedList} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" render={ (props) => {
+          return(<Movie {...props} addToSavedList={this.addToSavedList}/>)
+        }} />
       </div>
     )
   }
